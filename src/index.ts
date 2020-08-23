5badb76e077d0dc59af27283ea1f320a6b789641
Nicole Dong <xndong1020@hotmail.com>
2020-08-23 10:07:43 -0400

    note

--- src/index.ts
+++ src/index.ts
@@ -7,16 +7,24 @@ import mikroConfig from "./mikro-orm.config";
 
 const main = async () => {
   try {
+    // init database based on config file
     const orm = await MikroORM.init(mikroConfig);
+
+    // migrate database automatically rather than manually run it in cli
     await orm.getMigrator().up();
 
+    // create an Post object, and use entityManater to inject into data
+    // note: here the data is not saved into db yet.
     // const post = orm.em.create(Post, { title: "my first post 222" });
+
+    // commit to database
     // await orm.em.persistAndFlush(post);
 
+    // query
     const posts = await orm.em.find(Post, {});
     console.log("posts", posts);
   } catch (error) {
-    console.log("Jeremy error", error);
+    console.log("error", error);
   }
 };
 
